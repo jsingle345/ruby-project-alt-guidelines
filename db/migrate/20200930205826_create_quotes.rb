0a2a2eec5398a482a882012551c0d9faee7af86e
jsingle345 <jsingle345@gmail.com>
2020-09-30 16:36:41 -0500

    Added Users and Quotes Table

---
+++ db/migrate/20200930205826_create_quotes.rb
@@ -0,0 +1,10 @@
+class CreateQuotes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :quotes do |t|
+      t.string :quotation
+      t.string :mood
+      t.string :author
+      t.integer :user_id
+    end
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+User.create(name: "Joshua")
+User.create(name: "Katherine")
+User.create(name: "Caleb")
+User.create(name: "Donald")
+User.create(name: "Katrina")
+
+Quote.create(quotation: "Life is a box of Chocolate", mood: "worried", author: "Forest Gump")
+Quote.create(quotation: "It is what it is...", mood: "carefree", author: "Unknown")
+Quote.create(quotation: "You don't work you don't eat", mood: "lazy", author: "Momma Singleton")
+Quote.create(quotation: "Two ways to every coin", mood: "confused", author: "Unknown")
